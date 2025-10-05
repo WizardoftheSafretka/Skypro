b54c20ffd56c369f840f682402da26e0d705ea2b
Wizard <merihov2yandex.ru>
2025-10-05 14:41:23 +0300

    complete widget.py

--- src/widget.py
+++ src/widget.py
@@ -7,13 +7,16 @@ def mask_account_card(type_card_number: str) -> str:
     """Функция, которая маскирует номер карты или счета"""
 
     type_card_number_list = type_card_number.split()
-    type = re.search('[a-zA-Z]', type_card_number)
-    number = type_card_number_list[1]
-    if type == 'Счет':
-        return f'{type}: {get_mask_account(number)}'
+    new_list_alpha = []
+    new_list_number = []
+    result_number = ''
+    for i in type_card_number_list:
+        if i.isalpha():
+            new_list_alpha.append(i)
+        else:
+            new_list_number.append(i)
+    if 'Счет' in new_list_alpha:
+        result_number += get_mask_account(''.join(new_list_number))
     else:
-        return f'{type}: {mask_account_card(number)}'
-
-
-if __name__ == '__main__':
-    print(mask_account_card('Maestro 1596837868705199'))
+        result_number += get_mask_card_number(''.join(new_list_number))
+    return f'{' '.join(new_list_alpha)} {result_number}'
