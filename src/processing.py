c2c17a15478a92bf9a1d8bb8d4254a0aea17812b
Wizard <merihov2yandex.ru>
2025-10-27 19:49:36 +0300

    isort flake8 mypy new func processing.py

--- src/processing.py
+++ src/processing.py
@@ -12,4 +12,4 @@ def filter_by_state(list_dict: list[dict], state: str = "EXECUTED") -> list[dict
 def sort_by_date(list_dict: list[dict], reverse_order: bool = True) -> list[dict]:
     """Функция сортировки списка словарей по дате (по умолчанию - убывание)"""
 
-    return  sorted(list_dict, key=lambda x: x["date"], reverse=reverse_order)
+    return sorted(list_dict, key=lambda x: x["date"], reverse=reverse_order)
