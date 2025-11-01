f45387eec018d1c1f526f28765563b65c7f0045b
Wizard <merihov2yandex.ru>
2025-11-01 19:08:08 +0300

    fix processing.py,  test_processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,10 @@
-def filter_by_state(list_dict: list[dict], state: str = "EXECUTED") -> list[dict]:
+def filter_by_state(operations: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функия, принимающая список словарей и возвращающая новый список словарей с ключем state"""
 
-    new_list = []
-    for i in list_dict:
-        if i.get("state") == state:
-            new_list.append(i)
-
-    return new_list
+    return [oper_ for oper_ in operations if oper_["state"] == state]
 
 
-def sort_by_date(list_dict: list[dict], reverse_order: bool = True) -> list[dict]:
+def sort_by_date(operations: list[dict], reverse_order: bool = True) -> list[dict]:
     """Функция сортировки списка словарей по дате (по умолчанию - убывание)"""
 
-    return sorted(list_dict, key=lambda x: x["date"], reverse=reverse_order)
+    return sorted(operations, key=lambda x: x["date"], reverse=reverse_order)
--- test/test_processing.py
+++ test/test_processing.py
@@ -3,50 +3,41 @@ from src.processing import filter_by_state
 from src.processing import sort_by_date
 
 @pytest.fixture()
-def filter_by_state_normal():
-    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+def operations():
+    return [
+        {"id": 1, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 2, "state": "CANCELED", "date": "2016-07-03T18:35:29.512364"},
+        {"id": 3, "state": "EXECUTED", "date": "2015-07-03T18:35:29.512364"},
+    ]
 
-@pytest.fixture()
-def filter_by_state_without_state():
-    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'EXECUTED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'EXECUTED', 'date': '2018-10-14T08:21:33.419441'}]
+@pytest.mark.parametrize("state, expected", [
+    ("EXECUTED", [
+        {"id": 1, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 3, "state": "EXECUTED", "date": "2015-07-03T18:35:29.512364"}
+    ]),
+    ("CANCELED", [
+        {"id": 2, "state": "CANCELED", "date": "2016-07-03T18:35:29.512364"},
+    ])
+])
+def test_filter_by_state(state, expected, operations):
+    result = filter_by_state(operations, state=state)
+    assert result == expected
 
-@pytest.fixture()
-def sort_by_date_normal():
-    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+@pytest.mark.parametrize("reverse_order, expected", [
+    (True, [
+        {"id": 1, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 2, "state": "CANCELED", "date": "2016-07-03T18:35:29.512364"},
+        {"id": 3, "state": "EXECUTED", "date": "2015-07-03T18:35:29.512364"},
+    ]),
+    (False, [
+        {"id": 3, "state": "EXECUTED", "date": "2015-07-03T18:35:29.512364"},
+        {"id": 2, "state": "CANCELED", "date": "2016-07-03T18:35:29.512364"},
+        {"id": 1, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ])
+])
+def test_sort_by_date(reverse_order, expected, operations):
+    result = sort_by_date(operations, reverse_order=reverse_order)
+    assert result == expected
 
-@pytest.fixture()
-def sort_by_date_unnormal():
-    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03018:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-@pytest.fixture()
-def sort_by_date_equal():
-    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'}]
-
-@pytest.mark.parametrize("x, y, expected", [(filter_by_state_normal, 'EXECUTED', [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
-                                         (filter_by_state_normal, 'CANCELED', [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]),
-                                         (filter_by_state_without_state, 'CANCELED', [])])
-def filter_by_state(x, y, expected):
-    assert filter_by_state(x, y) == expected
-
-def test_sort_by_date_normal(sort_by_date_normal):
-    assert sort_by_date(sort_by_date_normal) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-
-
-def test_sort_by_date_reverse(sort_by_date_normal):
-    assert sort_by_date(sort_by_date_normal, False) == [{'date': '2018-06-30T02:08:58.425572', 'id': 939719570, 'state': 'EXECUTED'},
- {'date': '2018-09-12T21:27:25.241689', 'id': 594226727, 'state': 'CANCELED'},
- {'date': '2018-10-14T08:21:33.419441', 'id': 615064591, 'state': 'CANCELED'},
- {'date': '2019-07-03T18:35:29.512364', 'id': 41428829, 'state': 'EXECUTED'}]
-
-def test_sort_by_date_equal(sort_by_date_equal):
-    assert sort_by_date(sort_by_date_equal) == [{'date': '2019-07-03T18:35:29.512364', 'id': 41428829, 'state': 'EXECUTED'},
- {'date': '2019-07-03T18:35:29.512364', 'id': 939719570, 'state': 'EXECUTED'},
- {'date': '2019-07-03T18:35:29.512364', 'id': 594226727, 'state': 'CANCELED'},
- {'date': '2019-07-03T18:35:29.512364', 'id': 615064591, 'state': 'CANCELED'}]
-
-def test_sort_by_date_unnormal():
-    with pytest.raises(ValueError) as exc_info:
-        sort_by_date(sort_by_date_unnormal)
-    assert str(exc_info.value) == "Некорректный формат даты"
 
 
