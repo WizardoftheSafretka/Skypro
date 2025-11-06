f7e5283599f5dbb1b8ff95ffcb9be9a2da5ead37
Wizard <merihov2yandex.ru>
2025-11-06 20:44:14 +0300

    add description of func filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -1,6 +1,9 @@
 from typing import Iterator
 
 def filter_by_currency(transactions: list[dict], currency: str) -> Iterator:
+    """Функция, которая должна возвращать итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной (например, USD)"""
+
+
     while True:
         yield (transaction for transaction in transactions if transaction["operationAmount"]["currency"]["name"] == currency)
 
