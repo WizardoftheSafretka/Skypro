9a0a076cbd6abf11609867e499b7239c4a73b737
Wizard <merihov2yandex.ru>
2025-11-07 18:04:07 +0300

    update generators.py and test_generators.py add func transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -7,3 +7,10 @@ def filter_by_currency(transactions: list, currency: str) -> iter:
     for transaction in (transaction for transaction in transactions if transaction["operationAmount"]["currency"]["name"] == currency):
         yield transaction
 
+
+def transaction_descriptions(transactions: list) -> iter:
+    """Генератор, который принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
+
+    for transaction in transactions:
+        if 'description' in transaction:
+            yield transaction['description']
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,19 +1,27 @@
 import pytest
-from src.generators import filter_by_currency
+from src.generators import filter_by_currency, transaction_descriptions
 
 @pytest.fixture()
 def transactions_currency():
-    return [{"id": 1, "operationAmount": {"currency": {"name": "USD"}}},
-            {"id": 2, "operationAmount": {"currency": {"name": "USD"}}},
-            {"id": 3, "operationAmount": {"currency": {"name": "руб."}}},
-            {"id": 4, "operationAmount": {"currency": {"name": "руб."}}},
+    return [{"id": 1, "description": "Перевод организации", "operationAmount": {"currency": {"name": "USD"}}},
+            {"id": 2, "description": "Перевод со счета на счет", "operationAmount": {"currency": {"name": "USD"}}},
+            {"id": 3, "description": "Перевод со карты на карту", "operationAmount": {"currency": {"name": "руб."}}},
+            {"id": 4, "description": "Перевод со счета на счет", "operationAmount": {"currency": {"name": "руб."}}},
             ]
 
 
 def test_filter_by_currency_usd(transactions_currency):
     expected_result = filter_by_currency(transactions_currency, "USD")
-    assert (next(expected_result)) == {'id': 1, 'operationAmount': {'currency': {'name': 'USD'}}}, {'id': 2, 'operationAmount': {'currency': {'name': 'USD'}}}
+    assert (next(expected_result)) == {"id": 1, "description": "Перевод организации", "operationAmount": {"currency": {"name": "USD"}}}, {"id": 2, "description": "Перевод со счета на счет", "operationAmount": {"currency": {"name": "USD"}}}
 
 def test_filter_by_currency_rub(transactions_currency):
     expected_result = filter_by_currency(transactions_currency, "руб.")
-    assert (next(expected_result)) == {"id": 3, "operationAmount": {"currency": {"name": "руб."}}}, {"id": 4, "operationAmount": {"currency": {"name": "руб."}}}
+    assert (next(expected_result)) == {"id": 3, "description": "Перевод со карты на карту", "operationAmount": {"currency": {"name": "руб."}}}, {"id": 4, "description": "Перевод со счета на счет", "operationAmount": {"currency": {"name": "руб."}}}
+
+def test_transaction_descriptions(transactions_currency):
+    expected_result = transaction_descriptions(transactions_currency)
+    assert (next(expected_result)) == "Перевод организации"
+    assert (next(expected_result)) == "Перевод со счета на счет"
+    assert (next(expected_result)) == "Перевод со карты на карту"
+    assert (next(expected_result)) == "Перевод со счета на счет"
+
