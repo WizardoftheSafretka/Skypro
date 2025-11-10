2199d5b2594de2ecb274d87678041c8114286e3a
Wizard <merihov2yandex.ru>
2025-11-10 20:30:10 +0300

    isort, black, flake8, mypy .

--- src/decorators.py
+++ src/decorators.py
@@ -1,8 +1,9 @@
-from typing import Callable, Any
+from typing import Any, Callable
+
 
 def log(filename: str | None = None) -> Callable:
     def decorator(func: Callable) -> Any:
-        def wrapper(*args, **kwargs) -> Any:
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 result = func(*args, **kwargs)
                 massage = f"Функция {func.__name__} выполнена!"
@@ -13,13 +14,17 @@ def log(filename: str | None = None) -> Callable:
                     print(massage)
                 return result
             except Exception as e:
-                msg = (f"Функция {func.__name__} не выполнена! Произошла ошибка {type(e).__name__}: {e},"
-                       f"входные параметры: {args, kwargs}.")
+                msg = (
+                    f"Функция {func.__name__} не выполнена! Произошла ошибка {type(e).__name__}: {e},"
+                    f"входные параметры: {args, kwargs}."
+                )
                 if filename:
                     with open(filename, "a", encoding="utf-8") as f:
                         f.write(msg + "\n")
                 else:
-                     print(massage)
+                    print(massage)
                 raise
+
         return wrapper
+
     return decorator
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,6 +1,7 @@
 import pathlib
 
 import pytest
+
 from src.decorators import log
 
 
@@ -9,10 +10,12 @@ def test_success_log_in_console(capsys):
     @log()
     def foo(a, b):
         return a + b
+
     result = foo(2, 3)
     assert result == 5
     capture = capsys.readouterr()
     assert capture.out == "Функция foo выполнена!\n"
+
 
 def test_error_in_file(tmp_path: pathlib.Path):
     log_file = tmp_path / "log.txt"
@@ -25,6 +28,6 @@ def test_error_in_file(tmp_path: pathlib.Path):
         foo_2(1, "2")
 
     content = log_file.read_text(encoding="utf-8")
-    assert content == "Функция foo_2 не выполнена! Произошла ошибка TypeError: unsupported operand type(s) for +: 'int' and 'str',входные параметры: ((1, '2'), {}).\n"
-
-
+    assert "Функция foo_2 не выполнена!" in content
+    assert "TypeError: unsupported operand type(s)" in content
+    assert "входные параметры: ((1, '2'), {})" in content
