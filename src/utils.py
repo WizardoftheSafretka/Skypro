95a1afd7c224997974ae0895f49e80f9400ba015
Wizard <merihov2yandex.ru>
2025-11-13 19:42:55 +0300

    create utils and add func get_list_dict_about...

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+import json
+def get_list_dict_about_trans_json(file_name:str) -> list[dict]:
+    """Функция, возвращает список словарей с данными о финансовых транзакциях из JSON-файла"""
+    try:
+        with open(file_name, encoding='utf-8') as f:
+            data = json.load(f)
+        result = data
+        return result
+    except FileNotFoundError:
+        print("Нет возможности открыть файл")
+        return []
+
