eb595c786fc37dbfee30c6b5d64278932705ea41
Wizard <merihov2yandex.ru>
2025-11-22 16:37:54 +0300

    mypy isort flake8 black

--- src/transactions.py
+++ src/transactions.py
@@ -1,10 +1,14 @@
+from typing import Any, Hashable
+
 import pandas as pd
 
-def read_transactions_csv(filename: str) -> list[dict]:
+
+def read_transactions_csv(filename: str) -> dict[Hashable, Any]:
     """Считывание файла csv"""
 
     read_csv = pd.read_csv(filename)
     return read_csv.to_dict()
+
 
 def read_transactions_excel(filename: str) -> list[dict]:
     """Считывание файла excel"""
--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -1,9 +1,11 @@
 from unittest.mock import patch
+
 from src.transactions import read_transactions_csv, read_transactions_excel
+
 
 @patch("pandas.read_csv")
 def test_read_transactions_csv(mock_csv):
-    expected = [{"a":"b"}]
+    expected = [{"a": "b"}]
     mock_csv.return_value.to_dict.return_value = expected
     result = read_transactions_csv("test_csv")
     assert result == expected
@@ -11,7 +13,7 @@ def test_read_transactions_csv(mock_csv):
 
 @patch("pandas.read_excel")
 def test_read_transactions_excel(mock_excel):
-    expected = [{"a":"b"}]
+    expected = [{"a": "b"}]
     mock_excel.return_value.to_dict.return_value = expected
     result = read_transactions_excel("test_excel")
     assert result == expected
