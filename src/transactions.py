251e7048fa9ddfc8b49e83079bacea74ee46c893
Wizard <merihov2yandex.ru>
2025-11-22 16:27:17 +0300

    create transactions.py and test_transactions.py

---
+++ src/transactions.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def read_transactions_csv(filename: str) -> list[dict]:
+    """Считывание файла csv"""
+
+    read_csv = pd.read_csv(filename)
+    return read_csv.to_dict()
+
+def read_transactions_excel(filename: str) -> list[dict]:
+    """Считывание файла excel"""
+
+    read_excel = pd.read_excel(filename)
+    return read_excel.to_dict(orient="records")
