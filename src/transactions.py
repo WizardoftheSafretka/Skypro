616a02ebca2165dda4385f939857f375c37959de
Wizard <merihov2yandex.ru>
2025-11-23 19:19:03 +0300

    update transactions.py

--- src/transactions.py
+++ src/transactions.py
@@ -1,10 +1,11 @@
 import pandas as pd
+from black.linegen import delimiter_split
 
 
 def read_transactions_csv(filename: str) -> list[dict]:
     """Считывание файла csv"""
 
-    read_csv = pd.read_csv(filename)
+    read_csv = pd.read_csv(filename, delimiter = ";")
     return read_csv.to_dict(orient="records")
 
 
